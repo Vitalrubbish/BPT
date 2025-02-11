90559b4fb1da8e778d1a6e53f625e1322f349efc
Vitalrubbish <zxxzdq2006@sjtu.edu.cn>
2025-02-11 17:16:39 +0800

    对于cin, cout进行了优化

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "BPT.h"
 BPT <Data> bpt("file");
 int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
     int operation_cnt;
     std::cin >> operation_cnt;
     while (operation_cnt--) {
