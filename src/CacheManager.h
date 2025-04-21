8ed82976b48e765be4e6f7b39abcd25d042e794f
Vitalrubbish <zxxzdq2006@sjtu.edu.cn>
2025-04-21 08:26:24 +0800

    取消了哈希结构

---
+++ src/CacheManager.h
@@ -0,0 +1,12 @@
+#ifndef CACHEMANAGER_H
+#define CACHEMANAGER_H
+
+const int max_size_ = 50;
+const int k = 3;
+
+template <class T>
+class CacheManager {
+    T cachePool[max_size_];
+
+};
+#endif //CACHEMANAGER_H
